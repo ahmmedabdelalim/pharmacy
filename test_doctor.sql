918f85656cfa0e20dc0d15a24463b0c59cf9cb71
ahmmedabdelalim <am909962@gmail.com>
2022-04-15 14:46:32 +0200

    asdas

--- test_doctor.sql
+++ test_doctor.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 15, 2022 at 02:34 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `test_doctor`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
 
 CREATE TABLE `admins` (
   `id` bigint(20) UNSIGNED NOT NULL,
