ffefc449319c5a409948dfca668038a059625006
ahmmedabdelalim <am909962@gmail.com>
2022-04-21 14:30:57 +0200

    sf

--- resources/views/branch-views/dashboard.blade.php
+++ resources/views/branch-views/dashboard.blade.php
@@ -46,130 +46,21 @@
         <!-- End Page Header -->
 
         <!-- Card -->
-        {{-- <div class="card card-body mb-3 mb-lg-5">
-            <div class="row gx-2 gx-lg-3 mb-2">
-                <div class="col-9">
-                    <h4><i style="font-size: 30px"
-                           class="tio-chart-bar-4"></i>{{\App\CentralLogics\translate('dashboard_order_statistics')}}</h4>
-                </div>
-                <div class="col-3">
-                    <select class="custom-select" name="statistics_type" onchange="order_stats_update(this.value)">
-                        <option value="overall" {{session()->has('statistics_type') && session('statistics_type') == 'overall'?'selected':''}}>
-                            {{\App\CentralLogics\translate('overall_statistics')}}
-                        </option>
-                        <option value="today" {{session()->has('statistics_type') && session('statistics_type') == 'today'?'selected':''}}>
-                            {{\App\CentralLogics\translate("today's_statistics")}}
-                        </option>
-                        <option value="this_month" {{session()->has('statistics_type') && session('statistics_type') == 'this_month'?'selected':''}}>
-                            {{\App\CentralLogics\translate("this_month's_statistics")}}
-                        </option>
-                    </select>
-                </div>
-            </div>
-            <div class="row gx-2 gx-lg-3" id="order_stats">
-               
-            </div>
-        </div> --}}
+
         <!-- End Card -->
 
-        {{-- <div class="row gx-2 gx-lg-3">
-            <div class="col-lg-12 mb-3 mb-lg-12">
 
-                <!-- Card -->
-                <div class="card h-100">
-                    <!-- Body -->
-                    <div class="card-body">
-                        <div class="row mb-4">
-                            <div class="col-12 mb-3 border-bottom">
-                                <h5 class="card-header-title float-left mb-2">
-                                    <i style="font-size: 30px" class="tio-chart-pie-1"></i>
-                                    {{\App\CentralLogics\translate('earning_statistics_for_business_analytics')}}
-                                </h5>
-                                <!-- Legend Indicators -->
-                                <h5 class="card-header-title float-right mb-2">{{\App\CentralLogics\translate('monthly_earning')}}
-                                    <i style="font-size: 30px" class="tio-chart-bar-2"></i>
-                                </h5>
-                                <!-- End Legend Indicators -->
-                            </div>
-                            <div class="col-md-4 graph-border-1">
-                                <div class="mt-2 center-div">
-                                      <span class="h6 mb-0">
-                                          <i class="legend-indicator" style="background-color: #B6C867!important;"></i>
-                                         {{ \App\CentralLogics\translate('earnings') }} : {{ array_sum($earning) ." ".\App\CentralLogics\Helpers::currency_symbol() }}
-                                      </span>
-                                </div>
-                            </div>
-                        </div>
                         <!-- End Row -->
 
                         <!-- Bar Chart -->
-                        <div class="chartjs-custom">
-                            <canvas id="updatingData" style="height: 20rem;"
-                                    data-hs-chartjs-options='{
-                            "type": "bar",
-                            "data": {
-                              "labels": ["Jan","Feb","Mar","April","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
-                              "datasets": [
-                             {
-                                "data": [{{$earning[1]}},{{$earning[2]}},{{$earning[3]}},{{$earning[4]}},{{$earning[5]}},{{$earning[6]}},{{$earning[7]}},{{$earning[8]}},{{$earning[9]}},{{$earning[10]}},{{$earning[11]}},{{$earning[12]}}],
-                                "backgroundColor": "#B6C867",
-                                "borderColor": "#B6C867"
-                              }]
-                            },
-                            "options": {
-                              "scales": {
-                                "yAxes": [{
-                                  "gridLines": {
-                                    "color": "#e7eaf3",
-                                    "drawBorder": false,
-                                    "zeroLineColor": "#e7eaf3"
-                                  },
-                                  "ticks": {
-                                    "beginAtZero": true,
-                                    "stepSize": 50000,
-                                    "fontSize": 12,
-                                    "fontColor": "#97a4af",
-                                    "fontFamily": "Open Sans, sans-serif",
-                                    "padding": 10,
-                                    "postfix": " {{ \App\CentralLogics\Helpers::currency_symbol() }}"
-                                  }
-                                }],
-                                "xAxes": [{
-                                  "gridLines": {
-                                    "display": false,
-                                    "drawBorder": false
-                                  },
-                                  "ticks": {
-                                    "fontSize": 12,
-                                    "fontColor": "#97a4af",
-                                    "fontFamily": "Open Sans, sans-serif",
-                                    "padding": 5
-                                  },
-                                  "categoryPercentage": 0.5,
-                                  "maxBarThickness": "10"
-                                }]
-                              },
-                              "cornerRadius": 2,
-                              "tooltips": {
-                                "prefix": " ",
-                                "hasIndicator": true,
-                                "mode": "index",
-                                "intersect": false
-                              },
-                              "hover": {
-                                "mode": "nearest",
-                                "intersect": true
-                              }
-                            }
-                          }'></canvas>
-                        </div>
+
                         <!-- End Bar Chart -->
                     </div>
                     <!-- End Body -->
                 </div>
                 <!-- End Card -->
             </div>
-        </div> --}}
+        </div> 
         <!-- End Row -->
     </div>
 @endsection
